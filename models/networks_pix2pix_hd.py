7606af1a956afa1f4e9a2893a5bb3abe360c130a
Phi Nguyễn <phi.nguyen.uet@gmail.com>
2022-04-21 15:48:43 +0700

    change to spectral_norm

--- models/networks_pix2pix_hd.py
+++ models/networks_pix2pix_hd.py
@@ -9,11 +9,13 @@ import numpy as np
 # Functions
 ###############################################################################
 from models.networks import SpectralNorm
-
+import inspect
 
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
+        print(inspect.getfile(m.__class__))
+
         m.weight.data.normal_(0.0, 0.02)
     elif classname.find('BatchNorm2d') != -1:
         m.weight.data.normal_(1.0, 0.02)
